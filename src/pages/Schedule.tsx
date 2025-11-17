96b4a37f06aaaf71676a72f62313322b219eb6f4
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-17 17:17:53 +0200

    feat(schedule): add Select component with daysOfWeek, form validation and demo search handler

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -3,13 +3,36 @@ import { useNavigate } from 'react-router-dom';
 import Button from "../components/UI/Button";
 import Card from "../components/UI/Card";
 import Title from "../components/UI/Title";
+import Label from "../components/UI/Label";
+import Select from "../components/UI/Select";
+import { daysOfWeek } from "../data/days";
 
 function Schedule() {
+    const [selectedDay, setSelectedDay] = React.useState<string>("");
+
     const nav = useNavigate();
+
+    function handleSearch() {
+        if (!selectedDay) {
+            alert("Please, fill all fields");
+            return;
+        }
+
+        alert("Hello, world!");
+    }
 
     return (
         <Card>
             <Title title={"Hello, schedule!"}/>
+
+            <div style={{marginTop: "30px"}}/>
+
+            <Label label={"Оберіть день (demo)"}/>
+            <Select value={selectedDay} onChange={setSelectedDay} options={daysOfWeek}/>
+
+            <div style={{marginTop: "20px"}}/>
+
+            <Button text="Search" onClick={handleSearch} />
             <Button text="To home" onClick={() => nav("/")} />
         </Card>
     )

aa0b5970ad38ca6e59210b204d4baa7526e376f0
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-17 13:33:35 +0200

    style(schedule): replace div with Card in Home to unify layout and design

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from "../components/UI/Button";
+import Card from "../components/UI/Card";
 
 function Schedule() {
     const nav = useNavigate();
 
     return (
-        <div style={{ textAlign: "center", marginTop: "80px" }}>
-            <h1>Hello, Schedule!</h1>
+        <Card>
+            <h2 style={{
+                textAlign: "center",
+                marginBottom: "30px",
+                color: "#1f2937",
+                fontSize: "24px"
+            }}>
+                Hello, schedule!
+            </h2>
             <Button text="To home" onClick={() => nav("/")} />
-        </div>
+        </Card>
     )
 }
 

efe2b81962c85eb69dc49a46d64b1dd874acc488
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-19 00:06:16 +0200

    feat(auth): add login handling logic, integrate loginRequest service and store JWT token in localStorage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,19 +5,31 @@ import Card from "../components/UI/Card";
 import Button from "../components/UI/Button";
 import Label from "../components/UI/Label";
 import Input from "../components/UI/Input";
+import loginRequest from "../services/Auth";
+import {LoginRequest} from "../types/LoginRequest";
 
 function Login() {
     const nav = useNavigate();
 
-    const [login, setLogin] = React.useState("");
+    const [username, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
 
-    function handleLogin() {
-        if (!login || !password) {
+    async function handleLogin() {
+        if (!username || !password) {
             alert("Please, fill all fields...");
             return;
         }
-        alert("Submit");
+
+        try {
+            const data: LoginRequest = ({ username: username, password: password })
+            const response = await loginRequest(data);
+
+            localStorage.setItem("authToken", response.token);
+
+            nav("/admin");
+        } catch (error: any) {
+            alert(error instanceof Error ? error.message : error);
+        }
     }
 
     return (
@@ -27,7 +39,7 @@ function Login() {
             <div style={{marginTop: "20px"}} />
 
             <Label label={"Введіть логін"} />
-            <Input type={"text"} value={login} onChange={setLogin} />
+            <Input type={"text"} value={username} onChange={setUsername} />
 
             <div style={{marginTop: "20px"}} />
 

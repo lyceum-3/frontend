b108773991a60e2f200ca1685e648558cf8f6049
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-18 20:08:58 +0200

    feat(router): add /login to AppRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Schedule from "../pages/Schedule";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
     return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/schedule" element={<Schedule />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );

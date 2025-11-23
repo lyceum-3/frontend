7533d1c3351a82927d06256703823b3636b116ba
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-23 23:31:19 +0200

    refactor(Home): improve greeting logic and enhance semantic structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,31 +3,36 @@ import { useNavigate } from "react-router-dom"
 import Button from "../components/UI/Button";
 import Card from "../components/UI/Card";
 import Title from "../components/UI/Title";
+import Subtitle from '../components/UI/Subtitle';
 
 function Home() {
     const nav = useNavigate();
 
     function getGreetings(): string {
-        const now = new Date().toLocaleString("en-US", { timeZone: "Europe/Kyiv" });
-        const hours = new Date(now).getHours();
+        const hourStr = new Intl.DateTimeFormat('en-US', {
+          hour: 'numeric',
+          hour12: false,
+          timeZone: 'Europe/Kyiv'
+        }).format(new Date());
+        const hours = Number(hourStr);
 
-        if (hours > 5 && hours <= 12) {
-            return "Доброго ранку 🌅";
-        } else if (hours >= 12 && hours < 18) {
-            return "Доброго дня ☀️";
-        } else if (hours >= 18 && hours < 24) {
-            return "Доброго вечора 🌙";
-        } else {
-            return "Доброї ночі 💤";
-        }
-    }
+        if (hours >= 6 && hours < 12) return 'Доброго ранку 🌅';
+        if (hours >= 12 && hours < 18) return 'Доброго дня ☀️';
+        if (hours >= 18 && hours < 24) return 'Доброго вечора 🌙';
+        return 'Доброї ночі 💤';
+      }
 
     return (
         <Card>
-            <Title title={getGreetings()}/>
-            <Button text="Розклад" onClick={() => {nav("/schedule")}} />
-            <Button text="Адмін панель" onClick={() => {nav("/admin")}} />
-            <Button text="Актовий зал" onClick={() => {nav("/events")}} />
+            <header style={{textAlign: "center", marginBottom: "20px"}}>
+                <Title title={getGreetings()}/>
+                <Subtitle>Портал Березанського ліцею №3</Subtitle>
+            </header>
+            <nav>
+                <Button text="Розклад" onClick={() => {nav("/schedule")}} />
+                <Button text="Адмін панель" onClick={() => {nav("/admin")}} />
+                <Button text="Актовий зал" onClick={() => {nav("/events")}} />
+            </nav>
         </Card>
     )
 }

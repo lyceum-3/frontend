b2bfd09c24bfa5b2270c3523101b6676ff2a575c
Andriy <114863893+noinsts@users.noreply.github.com>
2025-12-02 10:25:51 +0200

    feat(components): add Separator component for separate content

---
+++ src/components/UI/Separator.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Separator: FC = () => {
+    return (
+        <hr style={{ border: "none", borderTop: "1px solid #e5e7eb", margin: "15px 0" }} />
+    )
+}
+
+export default Separator;
